1b2a196e0b6802060a6a3a8a4fd4993e0044b6ec
DeGraciaMathieu <degracia.mathieu@gmail.com>
2025-05-28 21:35:58 +0200

    Configures session and Redis cache storage
    Configures the application to use both session and Redis
    cache for storing and retrieving data.
    
    It sets up routes to interact with both session and Redis
    cache, allowing to store and retrieve data using different keys
    for each application instance.

--- app-1/routes/web.php
+++ app-1/routes/web.php
@@ -1,8 +1,22 @@
 <?php
 
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    session()->put('app-1', 'app-1');
+
+    session()->put('test-1', 'Hello app-1!');
+
     dd(session()->all());
+
+});
+
+Route::get('redis', function () {
+
+    Cache::store('redis')->put('test-1', 'Hello app-1!');
+
+    dd(
+        Cache::store('redis')->get('test-1'),
+        Cache::store('redis')->get('test-2')
+    );
 });
--- app-2/routes/web.php
+++ app-2/routes/web.php
@@ -3,6 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    session()->put('app-2', 'app-2');
+
+    session()->put('test-2', 'Hello app-2!');
+
     dd(session()->all());
+});
+
+Route::get('redis', function () {
+
+    Cache::store('redis')->put('test-2', 'Hello app-2!');
+
+    dd(
+        Cache::store('redis')->get('test-1'),
+        Cache::store('redis')->get('test-2')
+    );
 });
