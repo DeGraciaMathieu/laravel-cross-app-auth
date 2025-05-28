8261196cf027b2162b96b869daa7d25d08bbca22
DeGraciaMathieu <degracia.mathieu@gmail.com>
2025-05-28 21:19:56 +0200

    Adds session data for each app
    Adds session data with unique keys for each application.
    
    This change allows verification of proper session isolation between the applications.

--- app-1/routes/web.php
+++ app-1/routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    session()->put('app-1', 'app-1');
+    dd(session()->all());
 });
--- app-2/routes/web.php
+++ app-2/routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    session()->put('app-2', 'app-2');
+    dd(session()->all());
 });
